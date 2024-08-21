41bb13453c283066d681ff3b55061d1fd142b348
Dawid Fourie <dawief4@gmail.com>
2024-08-21 11:04:05 -0700

    for real wtf?2

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT || 5000
 
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
 
